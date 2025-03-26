3399f5f975de7110c1206d3b3ccdf51fe9e8b235
osamos <ogundanajoseph17@gmail.com>
2025-03-26 19:16:11 +0100

    created new file for device sizes

---
+++ src/deviceSize.js
@@ -0,0 +1,5 @@
+export const deviceSizes = {
+  mobile: "480px",
+  tablet: "768px",
+  desktop: "1024px",
+};
--- src/theme.js
+++
@@ -1,6 +0,0 @@
-export const device = {
-    mobile: '(max-width: 600px)',
-    tablet: '(max-width: 868px)',
-    laptop: '(max-width: 1024px)',
-  };
-  
