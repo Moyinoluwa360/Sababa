0ad9c76551622d7685d43ccbaff616b2a4ce32d8
osamos <ogundanajoseph17@gmail.com>
2025-04-11 13:06:22 +0100

    The inputed removed most of the pages and imports in main.jsx and added them into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import './App.css'
-
+import './index.css'
+// router imports
+import { 
+  createBrowserRouter,
+  Route,
+  Routes,
+  NavLink,
+  Link
+ } from 'react-router-dom'
+// pages imports
+import StaticPageDiv from './staticPage'
+import HomePage from './Homepage/HomePage'
+import AllOutfits from './All Outfits/AllOutfits'
+import OutfitBreakdown from './Outfit Breakdown/OutfitBreakdown'
+import OutfitsPayment from './OutfitPayment/OutfitPayment'
+import ShoppingBag from './Bag/ShoppingBag'
+import CheckoutPage from './Checkout/Checkout'
+import ContactUs from './ContactUs/ContactUs'
+import AddCard from './Checkout/AddCard'
+import { AccountPage } from './Account/Account'
+import MobileAccountMenu from './Account/MobileAccountMenu'
+import SavedItems from './SavedItems/SavedItems'
+import { ForgotPassword } from './SignIn-Up/ForgotPassword/ForgotPassword'
+import SignUp from './SignIn-Up/signup'
 function App() {
 
-  return 
+  return(
+    <HomePage/>
+  ) 
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import StaticPageDiv from './staticPage'
-import HomePage from './Homepage/HomePage'
-import AllOutfits from './All Outfits/AllOutfits'
-import OutfitBreakdown from './Outfit Breakdown/OutfitBreakdown'
-import OutfitsPayment from './OutfitPayment/OutfitPayment'
-import ShoppingBag from './Bag/ShoppingBag'
-import CheckoutPage from './Checkout/Checkout'
-import ContactUs from './ContactUs/ContactUs'
-import AddCard from './Checkout/AddCard'
-import { AccountPage } from './Account/Account'
-import MobileAccountMenu from './Account/MobileAccountMenu'
-import SavedItems from './SavedItems/SavedItems'
-import { ForgotPassword } from './SignIn-Up/ForgotPassword/ForgotPassword'
-import SignUp from './SignIn-Up/signup'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    < SignUp/>
+    <App/>
   </StrictMode>
 )
