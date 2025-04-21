9af7d1022c445ed751de9921031e638dbee6ff57
osamos <ogundanajoseph17@gmail.com>
2025-04-21 20:22:39 +0100

    passed ishovered prop through navigation menu

--- src/Header/Navigation.jsx
+++ src/Header/Navigation.jsx
@@ -14,7 +14,7 @@ export const Navigation = () => {
         onMouseLeave={() => setIsHovered(false)}
       >
         <NavItem tabIndex="0">All Features <img src="dropdown.svg" alt="all feature drop down" /> </NavItem>
-        {isHovered && <NavigationMenu />}
+        {isHovered && <NavigationMenu setIsHovered={setIsHovered} />}
       </HoverWrapper>
       <NavItem tabIndex="0"> <NavLink to={"contact"}> Contact</NavLink> </NavItem>
     </Nav>
