daeec073c69e6805233584e6bc7a4308ded2e23d
osamos <ogundanajoseph17@gmail.com>
2025-07-31 20:11:56 +0100

    added "/" to the dropdown svg img src

--- src/pages/Header/Navigation.jsx
+++ src/pages/Header/Navigation.jsx
@@ -13,7 +13,7 @@ export const Navigation = () => {
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <NavItem tabIndex="0">All Features <img src="dropdown.svg" alt="all feature drop down" /> </NavItem>
+        <NavItem tabIndex="0">All Features <img src="/dropdown.svg" alt="all feature drop down" /> </NavItem>
         {isHovered && <NavigationMenu setIsHovered={setIsHovered} />}
       </HoverWrapper>
       <NavItem tabIndex="0"> <NavLink to={"contact"}> Contact Us</NavLink> </NavItem>
