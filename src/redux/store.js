cf4efc1560ecdc28faf819640fa1484ba770d9e0
osamos <ogundanajoseph17@gmail.com>
2025-07-31 21:27:32 +0100

    removed un used slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,11 @@
 // src/redux/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-import productReducer from "./slices/productSlice";
-import orderReducer from "./slices/orderSlice";
-import cartReducer from "./slices/cartSlice";
-import paymentReducer from "./slices/paymentSlice";
 import wishlistReducer from "./slices/wishlistSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    products: productReducer,
-    orders: orderReducer,
-    cart: cartReducer,
-    payments: paymentReducer,
     wishlist: wishlistReducer,
   },
   middleware: (getDefaultMiddleware) =>
