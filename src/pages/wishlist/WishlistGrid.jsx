bfaefb6b24c0a5ad4b52fd1c75919060b8615296
osamos <ogundanajoseph17@gmail.com>
2025-07-31 20:00:05 +0100

    created wishlist grid to hold wish list card and display them properly

---
+++ src/pages/wishlist/WishlistGrid.jsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import styled from "styled-components";
+import OutfitCard from "../../components/OutfitCard"
+
+export default function WhislistGrid({wish}) {
+  return (
+    <Wrapper>
+      {wish.map((outfit) => (
+          <OutfitCard 
+          key={outfit.id}
+          outfit = {outfit}
+        />
+      ))}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+  gap: 12px;
+  padding-left: 5px;
+  padding-right: 5px;  
+  @media (max-width: 480px){
+    gap: 12px;
+  }
+`;
+
