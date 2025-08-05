14a02209c99dc416fb5b4579812e2cca65536a38
osamos <ogundanajoseph17@gmail.com>
2025-08-05 16:12:17 +0100

    Added the Outfit reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import wishlistReducer from "./slices/wishlistSlice";
+import outfitsReducer from "./slices/outfitsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     wishlist: wishlistReducer,
+    outfits: outfitsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
