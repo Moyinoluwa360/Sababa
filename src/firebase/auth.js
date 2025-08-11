dcb196f9329aaaf85ed4937591a80668656ad94a
osamos <ogundanajoseph17@gmail.com>
2025-08-11 22:23:28 +0100

    made functions to create user with email and password also sign in function with email and password

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,19 +1,15 @@
 import { auth, db, googleAuthProvider } from "./firebase";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { store } from "../redux/store";
 import { setPostAuthData, clearUser, setError, setLoading } from "../redux/slices/authSlice";
+import { getUser } from "./firestore";
 
 // Auth state listener
 export const initAuthListener = () => {
-  return auth.onAuthStateChanged((user) => {
+  return auth.onAuthStateChanged(async (user) => {
     if (user) {
-      const userData = {
-        uid: user.uid,
-        email: user.email,
-        displayName: user.displayName,
-        photoURL: user.photoURL,
-      };
+       const userData = await getUser(user.uid);
       store.dispatch(setPostAuthData(userData));
     } else {
       store.dispatch(clearUser());
@@ -32,7 +28,7 @@ export const signInWithGoogle = async () => {
       uid: user.uid,
       email: user.email || "",
       displayName: user.displayName || "Anonymous",
-      createdAt: new Date(),
+      createdAt: new Date().toISOString(),
       photoURL: user.photoURL
     }, { merge: true });
 
@@ -47,6 +43,54 @@ export const signInWithGoogle = async () => {
     store.dispatch(setLoading(false));
   } catch (error) {
     store.dispatch(setError(error.message || "Google sign-in failed"));
+    store.dispatch(setLoading(false));
+  }
+};
+
+// createUserWithEmailAndPassword
+export const createUserWithEmail = async (email,password,displayName) => {
+  try {
+    store.dispatch(setLoading(true));
+    const result = await createUserWithEmailAndPassword(auth, email, password);
+    const user = result.user;
+
+    // Save user to Firestore
+    await setDoc(doc(db, "users", user.uid), {
+      uid: user.uid,
+      email: user.email || "",
+      displayName: displayName || "Anonymous",
+      createdAt: new Date().toISOString(),
+      photoURL: "default_profile_picture_url"
+    }, { merge: true });
+
+    const userData = {
+      uid: user.uid,
+      email: user.email,
+      displayName: displayName,
+      photoURL: user.photoURL,
+    };
+    store.dispatch(setPostAuthData(userData));
+    store.dispatch(setLoading(false));
+  } catch (error) {
+    store.dispatch(setError(error.message || "Failed to Create Account please validate your credentials and try again"));
+    store.dispatch(setLoading(false));
+  }
+};
+
+// signInWithEmailAndPassword
+export const signInWithEmail = async (email, password) => {
+  try {
+    store.dispatch(setLoading(true));
+    const result = await signInWithEmailAndPassword(auth, email, password);
+    const userId = result.user.uid;
+
+    // Fetch user data from Firestore
+    const userData = await getUser(userId);
+    
+    store.dispatch(setPostAuthData(userData));
+    store.dispatch(setLoading(false));
+  } catch (error) {
+    store.dispatch(setError(error.message || "Sign in failed"));
     store.dispatch(setLoading(false));
   }
 };
