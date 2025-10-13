fd7c363edd7df9c18f3e105e9c2cee1dbbff0f21
osamos <ogundanajoseph17@gmail.com>
2025-10-13 11:34:57 +0100

    handles the show modal for sign in

--- src/components/OutfitCard.jsx
+++ src/components/OutfitCard.jsx
@@ -1,70 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-// like buttton imports
 import LikeButton from './likeButton';
+import SignInModal from './SignInModal';
 import { Link } from 'react-router-dom';
 
-
 const OutfitCard = ({outfit, OOTDNUM}) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <Link 
-      to={`/home/alloutfits/${outfit.gender}/${outfit.id}`}
-      key={outfit.id}
-      state={{outfitNumber: OOTDNUM}}
-      style={{textDecoration:"none"}}
+    <>
+      <Link 
+        to={`/home/alloutfits/${outfit.gender}/${outfit.id}`}
+        key={outfit.id}
+        state={{outfitNumber: OOTDNUM}}
+        style={{textDecoration:"none"}}
       >
-      <CardWrapper>
-        <CardImageContainer>
-          <CardImage loading="lazy" src={outfit.outfitImage} alt={`OOTD ${outfit.id}`} />
-          <LikeButton
-            outfit = {outfit}
-            OOTDNUM = {OOTDNUM}
-            ariaLabel={`Like ${outfit.id}`}
-            bottom="12px"
-            right="12px"
-          />
-        </CardImageContainer>
-        {<OutfitTitle>OOTD {OOTDNUM}</OutfitTitle>}
-      </CardWrapper>
-    </Link>
+        <CardWrapper>
+          <CardImageContainer>
+            <CardImage loading="lazy" src={outfit.outfitImage} alt={`OOTD ${outfit.id}`} />
+            <LikeButton
+              outfit={outfit}
+              OOTDNUM={OOTDNUM}
+              ariaLabel={`Like ${outfit.id}`}
+              bottom="12px"
+              right="12px"
+              onShowModal={() => setShowModal(true)}
+            />
+          </CardImageContainer>
+          {<OutfitTitle>OOTD {OOTDNUM}</OutfitTitle>}
+        </CardWrapper>
+      </Link>
+
+      {/* Modal rendered outside Link */}
+      {showModal && (
+        <SignInModal 
+          open={showModal} 
+          setShowModal={setShowModal} 
+        />
+      )}
+    </>
   );
 };
 
 const CardWrapper = styled.article`
   display: flex;
   flex-direction: column;
-  min-width: 240px; /* Will fill grid cell */ /* Keeps card from getting too big */
-  max-width: 240px; /* Keeps card from getting too big */
+  min-width: 240px;
+  max-width: 240px;
   height: 391px;
   border-radius: 20px;
   @media (max-width: 550px){
     height: 307px;
-    min-width: 172px; /* Will fill grid cell */ /* Keeps card from getting too big */
+    min-width: 172px;
     max-width: 172px;
   }
   @media (max-width: 480px){
     height: 297px;
-    min-width: 172px; /* Will fill grid cell */ /* Keeps card from getting too big */
+    min-width: 172px;
     max-width: 172px;
   }
   @media (max-width: 410px){
     height: 277px;
-    min-width: 162px; /* Will fill grid cell */ /* Keeps card from getting too big */
+    min-width: 162px;
     max-width: 162px;
   }
   @media (max-width: 390px){
     height: 237px;
-    min-width: 142px; /* Will fill grid cell */ /* Keeps card from getting too big */
+    min-width: 142px;
     max-width: 142px;
   }
   @media (max-width: 355px){
     height: 197px;
-    min-width: 122px; /* Will fill grid cell */ /* Keeps card from getting too big */
+    min-width: 122px;
     max-width: 122px;
   }
   @media (max-width: 310px){
     height: 147px;
-    min-width: 92px; /* Will fill grid cell */ /* Keeps card from getting too big */
+    min-width: 92px;
     max-width: 92px;
   }
 `;
@@ -93,8 +105,6 @@ const CardImage = styled.img`
   object-position: center;
   border-radius: 20px;
 `;
-
-
 
 const OutfitTitle = styled.h3`
   color: #1C1C1C;
